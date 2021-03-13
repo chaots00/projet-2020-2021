330964bbc2f013d31a592762c9a17d7cec663ee3
chaots00 <ludovicmaster.com>
2021-03-13 20:45:17 +0100

    ceb

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ console.log( "hello world" );
 const connection = mysql.createConnection({
     host: '135.125.95.178',
     user: 'ludo',
-    password: `@C-UYa<6f\@jWyJ'`,
+    password: "@C-UYa<6f\\@jWyJ'",
     database: 'cyberbibli',
     
 });
