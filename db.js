b07f55b389fb5ac3b7a6a0215e0468efb63829ad
chaots00 <ludovicmaster.com>
2021-03-13 16:47:09 +0100

    motdepasstest

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: '135.125.95.178',
     user: 'ludo',
-    password: "@C-UYa<6f\\@jWyJ'",
+    password: "`@C-UYa<6f\@jWyJ'`",
     database: 'cyberbibli',
 });
 
